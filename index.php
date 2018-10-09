fee0b735ae8c9ff1672168d7da6060a23e0b7471
MayanthaJ <mayantha_j@yahoo.co.uk>
2018-10-09 18:21:20 +0530

    Google clinet object creation and set secrets

--- index.php
+++ index.php
@@ -8,5 +8,17 @@ session_start();
 $OAUTH2_CLIENT_ID = '511825270036-krnflkbigifkr2ddhs05q9anrspirg5m.apps.googleusercontent.com';
 $OAUTH2_CLIENT_SECRET = 'cOPtcCgfZKoWO7WhVb_z3h-5';
 
+//Object creation using Google client library 
+$client = new Google_Client();
+$client->setClientId($OAUTH2_CLIENT_ID);
+$client->setClientSecret($OAUTH2_CLIENT_SECRET);
+$client->setScopes('https://www.googleapis.com/auth/youtube');
+$redirect = filter_var('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'],
+  FILTER_SANITIZE_URL);
+$client->setRedirectUri($redirect);
+
+// Define an object that will be used to make all API requests.
+$youtube = new Google_Service_YouTube($client);
+
 
 ?>
