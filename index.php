1bdb07e27d23a7116f3ab467c839310961914739
MayanthaJ <mayantha_j@yahoo.co.uk>
2018-10-09 17:48:31 +0530

    Google client key and secret init

--- index.php
+++ index.php
@@ -3,4 +3,10 @@
 require_once __DIR__ . '/vendor/autoload.php';
 session_start();
 
+//initialize your google app client key and secret
+//Replace this with your own credentials
+$OAUTH2_CLIENT_ID = '511825270036-krnflkbigifkr2ddhs05q9anrspirg5m.apps.googleusercontent.com';
+$OAUTH2_CLIENT_SECRET = 'cOPtcCgfZKoWO7WhVb_z3h-5';
+
+
 ?>
