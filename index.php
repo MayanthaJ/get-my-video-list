cf5af65f53fe004aea54c321dd830cfa9c72849b
MayanthaJ <mayantha_j@yahoo.co.uk>
2018-10-09 18:45:36 +0530

    Retrive video list

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ session_start();
 
 //initialize your google app client key and secret
 //Replace this with your own credentials
-$OAUTH2_CLIENT_ID = '511825270036-krnflkbigifkr2ddhs05q9anrspirg5m.apps.googleusercontent.com';
-$OAUTH2_CLIENT_SECRET = 'cOPtcCgfZKoWO7WhVb_z3h-5';
+$OAUTH2_CLIENT_ID = '511825270036-fnd8kd94f825pl1ag801bf2dukf6g530.apps.googleusercontent.com';
+$OAUTH2_CLIENT_SECRET = 'zxwXD0Ui9FRSZNkZSAGE6BQd';
 
 //Object creation using Google client library 
 $client = new Google_Client();
@@ -20,5 +20,86 @@ $client->setRedirectUri($redirect);
 // Define an object that will be used to make all API requests.
 $youtube = new Google_Service_YouTube($client);
 
+// Check if an auth token exists for the required scopes
+$tokenSessionKey = 'token-' . $client->prepareScopes();
+if (isset($_GET['code'])) {
+  if (strval($_SESSION['state']) !== strval($_GET['state'])) {
+    die('The session state did not match.');
+  }
 
+  $client->authenticate($_GET['code']);
+  $_SESSION[$tokenSessionKey] = $client->getAccessToken();
+  header('Location: ' . $redirect);
+}
+
+if (isset($_SESSION[$tokenSessionKey])) {
+  $client->setAccessToken($_SESSION[$tokenSessionKey]);
+}
+
+// Check to ensure that the access token was successfully acquired.
+if ($client->getAccessToken()) {
+  try {
+    // Call the channels.list method to retrieve information about the
+    // currently authenticated user's channel.
+    $channelsResponse = $youtube->channels->listChannels('contentDetails', array(
+      'mine' => 'true',
+    ));
+
+    $htmlBody = '';
+    foreach ($channelsResponse['items'] as $channel) {
+      // Extract the unique playlist ID that identifies the list of videos
+      // uploaded to the channel, and then call the playlistItems.list method
+      // to retrieve that list.
+      $uploadsListId = $channel['contentDetails']['relatedPlaylists']['uploads'];
+
+      $playlistItemsResponse = $youtube->playlistItems->listPlaylistItems('snippet', array(
+        'playlistId' => $uploadsListId,
+        'maxResults' => 50
+      ));
+
+      $htmlBody .= "<h3>Videos in list $uploadsListId</h3><ul>";
+      foreach ($playlistItemsResponse['items'] as $playlistItem) {
+        $htmlBody .= sprintf('<li>%s (%s)</li>', $playlistItem['snippet']['title'],
+          $playlistItem['snippet']['resourceId']['videoId']);
+      }
+      $htmlBody .= '</ul>';
+    }
+  } catch (Google_Service_Exception $e) {
+    $htmlBody = sprintf('<p>A service error occurred: <code>%s</code></p>',
+      htmlspecialchars($e->getMessage()));
+  } catch (Google_Exception $e) {
+    $htmlBody = sprintf('<p>An client error occurred: <code>%s</code></p>',
+      htmlspecialchars($e->getMessage()));
+  }
+
+  $_SESSION[$tokenSessionKey] = $client->getAccessToken();
+} elseif ($OAUTH2_CLIENT_ID == 'REPLACE_ME') {
+  $htmlBody = <<<END
+  <h3>Client Credentials Required</h3>
+  <p>
+    You need to set <code>\$OAUTH2_CLIENT_ID</code> and
+    <code>\$OAUTH2_CLIENT_ID</code> before proceeding.
+  <p>
+END;
+} else {
+  $state = mt_rand();
+  $client->setState($state);
+  $_SESSION['state'] = $state;
+
+  $authUrl = $client->createAuthUrl();
+  $htmlBody = <<<END
+  <h3>Authorization Required</h3>
+  <p>You need to <a href="$authUrl">authorize access</a> before proceeding.<p>
+END;
+}
 ?>
+
+<!doctype html>
+<html>
+  <head>
+    <title>My Uploads</title>
+  </head>
+  <body>
+    <?=$htmlBody?>
+  </body>
+</html>
